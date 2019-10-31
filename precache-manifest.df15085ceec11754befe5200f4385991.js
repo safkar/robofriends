b2c0e7d85dea1a83ebfbf35e6aa89f121e7bf8f9
safkar <55669044+safkar@users.noreply.github.com>
2019-10-31 12:58:31 -0400

    Updates

---
+++ precache-manifest.df15085ceec11754befe5200f4385991.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaba45b7f37b8c9597baf80f402c1f34",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a3d33e9a29ef46169c6a",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "aff6d35187957c09c76e",
+    "url": "/robofriends/static/css/main.1a640899.chunk.css"
+  },
+  {
+    "revision": "a3d33e9a29ef46169c6a",
+    "url": "/robofriends/static/js/2.636021b4.chunk.js"
+  },
+  {
+    "revision": "aff6d35187957c09c76e",
+    "url": "/robofriends/static/js/main.47185165.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
